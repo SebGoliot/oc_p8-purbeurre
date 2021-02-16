d773ed931b03e5bbf831d686bbc3c785e10430ab
SebGoliot <50270166+SebGoliot@users.noreply.github.com>
2021-02-16 22:13:51 +0100

    Added a temporary product template and view

--- nutella/urls.py
+++ nutella/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, search
+from .views import index, search, product
 
 urlpatterns = [
     path('', index, name='index'),
     path('search', search, name='search'),
+    path('product', product, name='product'),
     path('', index, name='legal'),
 ]
--- nutella/views.py
+++ nutella/views.py
@@ -6,6 +6,24 @@ def index(request):
 
 
 
+def product(request):
+    query = request.GET.get('product_id')
+
+    ctx = {
+        'product_name': "Nutella",
+        'nutriscore': "c",
+        'product_img_url': 'https://static.openfoodfacts.org/images/products/301/762/040/6003/front_fr.130.200.jpg',
+        'product_reperes': [
+                "30.9 g Matières grasses / Lipides en quantité élevée",
+                "10.6 g Acides gras saturés en quantité élevée",
+                "56.3 g Sucres en quantité élevée",
+                "0.107 g Sel en faible quantité",
+                "Taille d'une portion : 15 g",
+            ],
+        }
+    return render(request, 'product.html', ctx)
+
+
 def search(request):
     query = request.GET.get('query')
     substitutes = get_products_from_search(query)
@@ -18,7 +36,6 @@ def search(request):
 
 
 def get_products_from_search(search) -> "list[dict]":
-    
     img_url = 'https://static.openfoodfacts.org/images/products/301/762/040/6003/front_fr.130.200.jpg'
 
     substitutes = []
