cc5d1351ffee054ea73c1a723451a68a195bd0ee
SebGoliot <50270166+SebGoliot@users.noreply.github.com>
2021-02-24 20:58:21 +0100

    Added an 'add_category' command

---
+++ nutella/management/commands/add_category.py
@@ -0,0 +1,45 @@
+from django.core.management.base import BaseCommand, CommandError
+from nutella.models import Category
+from django.db.utils import OperationalError
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'category_name',
+            nargs='+',
+            type=str, 
+            help="The name of the category to add"
+        )
+
+
+    def handle(self, *args, **options):
+        category_name = ' '.join(options['category_name'])
+
+        if self._name_is_available(category_name):
+            category = Category(name=category_name)
+            category.save()
+
+            self.stdout.write(self.style.SUCCESS(
+                (f"Successfuly created a new category: {category_name}\n"
+                "Please use `manage.py update_db` to populate this new category"
+                )
+            ))
+        else:
+            self.stdout.write(self.style.SUCCESS(
+                f"Looks like this category already exist: {category_name}"
+            ))
+            self.stdout.write(self.style.WARNING(
+                "Please use `manage.py update_db` to populate this category"
+            ))
+
+
+    def _name_is_available(self, category_name:str) -> bool:
+        try:
+            free = not Category.objects.filter(name = category_name).exists()
+        except OperationalError as err:
+            self.stdout.write(self.style.ERROR(
+                f"Could not create the category `{category_name}` !\n{err}"
+            ))
+            return False
+        return free
