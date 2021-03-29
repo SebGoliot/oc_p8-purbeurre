63d06bfb26a8718fe80a865426b5b63db4df5db6
SebGoliot <50270166+SebGoliot@users.noreply.github.com>
2021-03-30 01:11:13 +0200

    Added selenium tests
    Also updated .gitignore

---
+++ tests/selenium/test_forms.py
@@ -0,0 +1,69 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from accounts.models import CustomUser
+
+
+class TestForms(StaticLiveServerTestCase):
+
+    def setUp(self) -> None:
+        self.firstname = 'John'
+        self.lastname = 'Doe'
+        self.email = 'jdoe@gmail.com'
+        self.password = 'veab0toox*KASS.wrik'
+
+        self.selenium = webdriver.Chrome()
+        self.selenium.implicitly_wait(30)
+        self.selenium.set_page_load_timeout(30)
+
+
+    def test_signup_form(self):
+
+        self.selenium.get(f"{self.live_server_url}/signup/")
+
+        firstname = self.selenium.find_element_by_id('id_first_name')
+        lastname = self.selenium.find_element_by_id('id_last_name')
+        email = self.selenium.find_element_by_id('id_email')
+        password1 = self.selenium.find_element_by_id('id_password1')
+        password2 = self.selenium.find_element_by_id('id_password2')
+        submit = self.selenium.find_element_by_id('submit_button')
+
+        firstname.send_keys(self.firstname)
+        lastname.send_keys(self.lastname)
+        email.send_keys(self.email)
+        password1.send_keys(self.password)
+        password2.send_keys(self.password)
+        submit.send_keys(Keys.RETURN)
+
+        self.selenium.get(f"{self.live_server_url}/account/")
+
+        assert self.firstname in self.selenium.page_source
+        self.assertInHTML(self.email, self.selenium.page_source)
+
+
+    def test_login_form(self):
+
+        self.user = CustomUser.objects.create(
+            email=self.email, first_name=self.firstname)
+        self.user.set_password(self.password)
+        self.user.save()
+
+        self.selenium.get(f"{self.live_server_url}/login/")
+
+        email = self.selenium.find_element_by_id('id_username')
+        password1 = self.selenium.find_element_by_id('id_password')
+        submit = self.selenium.find_element_by_id('submit_button')
+
+        email.send_keys(self.email)
+        password1.send_keys(self.password)
+        submit.send_keys(Keys.RETURN)
+
+        self.selenium.get(f"{self.live_server_url}/account/")
+
+        assert self.firstname in self.selenium.page_source
+        assert self.email in self.selenium.page_source
+
+        CustomUser.objects.get(email=self.email).delete()
+
+        assert self.firstname in self.selenium.page_source
+        self.assertInHTML(self.email, self.selenium.page_source)
